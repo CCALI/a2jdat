c5e9379fa7688a3c414db289f5c9c169bc005e7c
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-05-07 10:25:31 -0500

    add styles back in and update css bundle path

--- src/util/get-css-bundle-path.js
+++ src/util/get-css-bundle-path.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = function () {
   const rootPath = path.join(__dirname, '..', '..')
 
-  return path.join(rootPath, 'dist', 'bundles', 'caja', 'server.css')
+  return path.join(rootPath, 'dist', 'bundles', 'a2jdat', 'server.css')
 }
---
+++ styles/styles.js
@@ -0,0 +1,3 @@
+import 'caja/styles/author.jquery-ui.css!';
+import 'caja/styles/jquery.fileupload-ui.css!';
+import 'caja/styles/A2J_Author.css!';
